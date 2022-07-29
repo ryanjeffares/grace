727b26684f43678c85d9a344a1a8d0a986caddc1
Ryan Jeffares <ryandjeffares@gmail.com>
2022-07-30 01:18:16 +0200

    updated run examples script

--- run_examples.py
+++ run_examples.py
@@ -7,6 +7,9 @@ for root, dirs, files in os.walk('./examples', topdown=True):
         example_path = os.path.join(root, file)
         print('Running', example_path)
         print()
-        os.system(f'./build/Release/grace {example_path}')
+        if os.name == 'nt':
+            os.system(f'./build/Release/Release/grace.exe {example_path}')
+        else:
+            os.system(f'./build/Release/grace {example_path}')
         print()
         print(f'Finished in {time.time() - start}')
